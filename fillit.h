089b5be374e997b0b7d91e06951aa778ea7ee756
Manon DELTOUR <deltour.manon.97@gmail.com>
2019-05-16 16:55:11 +0200

    depot Makefile auteur main fillit.h fillit.c

---
+++ fillit.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdeltour <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/02 18:34:39 by mdeltour          #+#    #+#             */
+/*   Updated: 2019/05/15 15:04:15 by mdeltour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include "libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <fcntl.h>
+
+int		ft_fillit(int fd);
+
+#endif
