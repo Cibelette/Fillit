76ffb454704574c7c82c6fdbfcbf4eca7556232e
Cibyl <cibyl@MacBook-Pro-de-Cibyl.local>
2019-06-13 15:02:41 +0200

    coucou

--- solve_map.c
+++ solve_map.c
@@ -3,104 +3,101 @@
 /*                                                        :::      ::::::::   */
 /*   solve_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdeltour <mdeltour@student.42.fr>          +#+  +:+       +#+        */
+/*   Bloc[1]: cibyl <cibyl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 15:46:12 by mdeltour          #+#    #+#             */
-/*   Updated: 2019/06/11 18:55:18 by mdeltour         ###   ########.fr       */
+/*   Updated: 2019/06/13 14:56:33 by cibyl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-#include <stdio.h>
 
-static int	can_place(t_tetris *curr, t_map *map, int x, int y)
+static int	can_place(t_tetris *curr, t_map *map, int loc[2])
 {
-	int		i;
-	int		j;
+
+	int		loc2[2];
 	int		count;
 
 	count = 0;
-	j = 0;
-	while (j < map->size && j < y + 4)
+	loc2[1] = 0;
+	while (loc2[1] < map->size && loc2[1] < loc[1] + 4)
 	{
-		i = 0;
-		while (i < map->size && i < x + 4 && (y + j) < map->size
-			&& (x + i) < map->size)
+		loc2[0] = 0;
+		while (loc2[0] < map->size && loc2[0] < loc[0] + 4 && (loc[1] + loc2[1]) < map->size
+			&& (loc[0] + loc2[0]) < map->size)
 		{
-			if (map->tab[y + j][x + i] >= 'A' && map->tab[y + j][x + i] <= 'Z'
-					&& curr->line[j][i] >= 'A' && curr->line[j][i] <= 'Z')
+			if (map->tab[loc[1] + loc2[1]][loc[0] + loc2[0]] >= 'A' && map->tab[loc[1] + loc2[1]][loc[0] + loc2[0]] <= 'Z'
+					&& curr->line[loc2[1]][loc2[0]] >= 'A' && curr->line[loc2[1]][loc2[0]] <= 'Z')
 				return (ERROR);
-			if (map->tab[y + j][x + i] == '.' && curr->line[j][i] >= 'A'
-					&& curr->line[j][i] <= 'Z')
+			if (map->tab[loc[1] + loc2[1]][loc[0] + loc2[0]] == '.' && curr->line[loc2[1]][loc2[0]] >= 'A'
+					&& curr->line[loc2[1]][loc2[0]] <= 'Z')
 				count++;
-			i++;
+			loc2[0]++;
 		}
-		j++;
+		loc2[1]++;
 	}
 	return (count != 4 ? ERROR : OK);
 }
 
-static int	place_tetri(t_tetris *curr, t_map *map, int x, int y)
+static int	place_tetri(t_tetris *curr, t_map *map, int loc[2])
 {
-	int		i;
-	int		j;
+	int		loc2[2];
 	int		count;
 
 	count = 0;
-	j = 0;
-	while (j < map->size && j < y + 4)
+	loc2[1] = 0;
+	while (loc2[1] < map->size && loc2[1] < loc[1] + 4)
 	{
-		i = 0;
-		while (i < map->size && i < x + 4 && (y + j) < map->size
-			&& (x + i) < map->size)
+		loc2[0] = 0;
+		while (loc2[0] < map->size && loc2[0] < loc[0] + 4 && (loc[1] + loc2[1]) < map->size
+			&& (loc[0] + loc2[0]) < map->size)
 		{
-			if (map->tab[y + j][x + i] >= 'A' && map->tab[y + j][x + i] <= 'Z'
-					&& curr->line[j][i] >= 'A' && curr->line[j][i] <= 'Z')
+			if (map->tab[loc[1] + loc2[1]][loc[0] + loc2[0]] >= 'A' && map->tab[loc[1] + loc2[1]][loc[0] + loc2[0]] <= 'Z'
+					&& curr->line[loc2[1]][loc2[0]] >= 'A' && curr->line[loc2[1]][loc2[0]] <= 'Z')
 				return (ERROR);
-			if (map->tab[y + j][x + i] == '.' && curr->line[j][i] >= 'A'
-					&& curr->line[j][i] <= 'Z')
+			if (map->tab[loc[1] + loc2[1]][loc[0] + loc2[0]] == '.' && curr->line[loc2[1]][loc2[0]] >= 'A'
+					&& curr->line[loc2[1]][loc2[0]] <= 'Z')
 			{
-				map->tab[y + j][x + i] = curr->line[j][i];
+				map->tab[loc[1] + loc2[1]][loc[0] + loc2[0]] = curr->line[loc2[1]][loc2[0]];
 				count++;
 				if (count == 4)
 					return (OK);
 			}
-			i++;
+			loc2[0]++;
 		}
-		j++;
+		loc2[1]++;
 	}
 	return (count != 4 ? ERROR : OK);
 }
 
-static int	try_place_block(t_tetris *curr, t_map *map, int x, int y)
+static int	try_place_block(t_tetris *curr, t_map *map, int loc[2])
 {
-	if (x > map->size || y > map->size)
+	if (loc[0] > map->size || loc[1] > map->size)
 		return (ERROR);
-	if (can_place(curr, map, x, y) == ERROR)
+	if (can_place(curr, map, loc) == ERROR)
 		return (ERROR);
-	if (place_tetri(curr, map, x, y) == ERROR)
+	if (place_tetri(curr, map, loc) == ERROR)
 		return (ERROR);
 	return (OK);
 }
 
 int			ft_solve(t_tetris *curr, t_map *map)
 {
-	int		x;
-	int		y;
+	int		loc[2];
 	char	**tmp;
 
 	if (!(tmp = (char **)malloc(sizeof(char *) * (map->size + 1))))
 		return (ERROR);
 	tmp[map->size] = NULL;
-	y = 0;
-	while (y < map->size)
+	loc[1] = 0;
+	while (loc[1] < map->size)
 	{
-		x = 0;
-		while (x < map->size)
+		loc[0] = 0;
+		while (loc[0] < map->size)
 		{
 			if (save_map(tmp, map->tab, map->size) == ERROR)
 				free(tmp);
-			if (try_place_block(curr, map, x, y) == OK && x < map->size)
+			if (try_place_block(curr, map, loc) == OK && loc[0] < map->size)
 			{
 				if (curr->next == NULL || ft_solve(curr->next, map) == OK)
 				{
@@ -110,9 +107,9 @@ int			ft_solve(t_tetris *curr, t_map *map)
 				if (save_map(map->tab, tmp, map->size) == ERROR)
 					return (ERROR);
 			}
-			x++;
+			loc[0]++;
 		}
-		y++;
+		loc[1]++;
 	}
 	free(tmp);
 	return (ERROR);
